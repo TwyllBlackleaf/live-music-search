126f460eed35455ca903fb79a2acbb453e9222a9
Liliana Cortes <liliana.cortes@pop.belmont.edu>
2021-05-28 16:55:04 -0500

    updated changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 //Google Maps API fetch & searchTerm Intergration 
 function searchLocal(){
-    var searchTerm = document.querySelector('#userInput').value;
+    var searchTermLocal = document.querySelector('#userInput').value;
 
     fetch(
         'https://www.google.com/maps/embed/v1/place?api_key=AIzaSyBcsR3u8CFQz51MueJdmvZvTyF8MWwvegw&q=' +
-        searchTerm
+        searchTermLocal
     )
         .then(function(response){
             return response.json();
@@ -16,23 +16,55 @@ function searchLocal(){
             mapImg.setAttribute('src', response.data.image_url);
             responseContainerEL.appendChild(mapImg);
         });
+
+        if (searchTermLocal.text) {
+            if (searchTermLocal.byBand) {
+                searchByBand();
+            } else if (searchTerm.byLocation) {
+                searchByLocation();
+            } else {
+                console.log("error, please choose band or location");
+            }
+        } else {
+            console.log("error, please enter a search term");
+        }
 }
+
+function addMarkerMap(){
+    const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let labelIndex = 0;
+
+    function initMap() {
+        const bangalore = { lat: 12.97, lng: 77.59 };
+        const map = new google.maps.Map(document.getElementById("map"), {
+         zoom: 12,
+         center: bangalore,
+    });
+  // This event listener calls addMarker() when the map is clicked.
+    google.maps.event.addListener(map, "click", (event) => {
+    addMarker(event.latLng, map);
+     });
+  // Add a marker at the center of the map.
+     addMarker(bangalore, map);
+}
+
+// Adds a marker to the map.
+function addMarker(location, map) {
+  // Add the marker at the clicked location, and add the next-available label
+  // from the array of alphabetical characters.
+  new google.maps.Marker({
+    position: location,
+    label: labels[labelIndex++ % labels.length],
+    map: map,
+  });
+}
+}
+
+
 var searchTerm = {
     text: "",
     byBand: false,
     byLocation: false,
-//modal errors for google map local 
-    //if (searchTerm.text) {
-       // if (searchTerm.byBand) {
-         //   searchByBand();
-       // } else if (searchTerm.byLocation) {
-         //   searchByLocation();
-       // } else {
-         //   console.log("error, please choose band or location");
-       // }
-   // } else {
-     //   console.log("error, please enter a search term");
-    //}
 };
 
 var resultsListEl = $("#results-list");
